5d1f2f7bcf2ef0854894d796a6070e67a4a2362b
Bugrahan OZTURK <sbozturk97@gmail.com>
2022-01-28 17:52:52 +0300

    second commit

--- app.js
+++ app.js
@@ -9,67 +9,76 @@
 const { Gateway, Wallets } = require('fabric-network');
 const FabricCAServices = require('fabric-ca-client');
 const path = require('path');
-const { buildCAClient, registerAndEnrollUser, enrollAdmin } = require('CAUtil.js');
-const { buildCCPOrg1, buildWallet } = require('AppUtil.js');
+const { buildCAClient, registerAndEnrollUser, enrollAdmin } = require('./CAUtil.js');
+const { buildCCPOrg1, buildWallet } = require('./AppUtil.js');
 
 const channelName = 'mychannel';
 const chaincodeName = 'erc20';
 const mspOrg1 = 'Org1MSP';
 const walletPath = path.join(__dirname, 'wallet');
-const org1UserId = 'appUser7';
+const org1UserId = 'appUser9';
+
+var ccp;
+var caClient;
+var wallet;
+var gateway;
+var network;
+var contract;
 
 function prettyJSONString(inputString) {
 	return JSON.stringify(JSON.parse(inputString), null, 2);
 }
 
-// pre-requisites:
-// - fabric-sample two organization test-network setup with two peers, ordering service,
-//   and 2 certificate authorities
-//         ===> from directory /fabric-samples/test-network
-//         ./network.sh up createChannel -ca
-// - Use any of the asset-transfer-basic chaincodes deployed on the channel "mychannel"
-//   with the chaincode name of "basic". The following deploy command will package,
-//   install, approve, and commit the javascript chaincode, all the actions it takes
-//   to deploy a chaincode to a channel.
-//         ===> from directory /fabric-samples/test-network
-//         ./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-javascript/ -ccl javascript
-// - Be sure that node.js is installed
-//         ===> from directory /fabric-samples/asset-transfer-basic/application-javascript
-//         node -v
-// - npm installed code dependencies
-//         ===> from directory /fabric-samples/asset-transfer-basic/application-javascript
-//         npm install
-// - to run this test application
-//         ===> from directory /fabric-samples/asset-transfer-basic/application-javascript
-//         node app.js
+exports.init_connection = async () => {
+	try {
+		
+		ccp = buildCCPOrg1();
+		caClient = buildCAClient(FabricCAServices, ccp, 'ca.org1.example.com');
+		wallet = await buildWallet(Wallets, walletPath);
+		await enrollAdmin(caClient, wallet, mspOrg1);
+		await registerAndEnrollUser(caClient, wallet, mspOrg1, org1UserId, 'org1.department1');
 
-// NOTE: If you see  kind an error like these:
-/*
-    2020-08-07T20:23:17.590Z - error: [DiscoveryService]: send[mychannel] - Channel:mychannel received discovery error:access denied
-    ******** FAILED to run the application: Error: DiscoveryService: mychannel error: access denied
+		gateway = new Gateway();
 
-   OR
+		await gateway.connect(ccp, {
+			wallet,
+			identity: org1UserId,
+			discovery: { enabled: true, asLocalhost: true } // using asLocalhost as this gateway is using a fabric network deployed locally
+		});
 
-   Failed to register user : Error: fabric-ca request register failed with errors [[ { code: 20, message: 'Authentication failure' } ]]
-   ******** FAILED to run the application: Error: Identity not found in wallet: appUser
-*/
-// Delete the /fabric-samples/asset-transfer-basic/application-javascript/wallet directory
-// and retry this application.
-//
-// The certificate authority must have been restarted and the saved certificates for the
-// admin and application user are not valid. Deleting the wallet store will force these to be reset
-// with the new certificate authority.
-//
+		// Build a network instance based on the channel where the smart contract is deployed
+		network = await gateway.getNetwork(channelName);
 
-/**
- *  A test application to show basic queries operations with any of the asset-transfer-basic chaincodes
- *   -- How to submit a transaction
- *   -- How to query and check the results
- *
- * To see the SDK workings, try setting the logging to show on the console before running
- *        export HFC_LOGGING='{"debug":"console"}'
- */
-async function main() {
+		// Get the contract from the network.
+		contract = network.getContract(chaincodeName);
+
+	} catch (error) {
+		console.error(`******** FAILED to initiliaze Hyperledger Fabric Connection: ${error}`);
+	}
+}
+
+exports.check_balance = async () => {
+	try{
+		console.log('\n--> Evaluate Transaction: ClientAccountBalance, function returns the balance of the given account');
+		let result = await contract.evaluateTransaction('ClientAccountBalance');
+		console.log(`*** Result: ${result}`);
+		return result.toString();
+	} catch(error) {
+		console.error(`******** FAILED to check the balance of the account: ${error}`);
+	}
+}
+
+exports.mint = async (amount) => {
+	try{
+		console.log('\n--> Submit Transaction: Mint, creates new tokens and adds them to minter\'s account balance');
+		let result = await contract.submitTransaction('Mint', amount);
+		console.log('*** Result: committed');
+	} catch(error) {
+		console.error(`******** FAILED to mint tokens to the account: ${error}`)
+	}
+}
+
+exports.test = async () => {
 	try {
 		// build an in memory object with the network configuration (also known as a connection profile)
 		const ccp = buildCCPOrg1();
@@ -122,66 +131,6 @@ async function main() {
 			let test = await contract.evaluateTransaction('ClientAccountID');
 			console.log(`*** Account_ID: ${test}`);
 
-			/*
-			// Initialize a set of asset data on the channel using the chaincode 'InitLedger' function.
-			// This type of transaction would only be run once by an application the first time it was started after it
-			// deployed the first time. Any updates to the chaincode deployed later would likely not need to run
-			// an "init" type function.
-			console.log('\n--> Submit Transaction: InitLedger, function creates the initial set of assets on the ledger');
-			await contract.submitTransaction('InitLedger');
-			console.log('*** Result: committed');
-
-			// Let's try a query type operation (function).
-			// This will be sent to just one peer and the results will be shown.
-			console.log('\n--> Evaluate Transaction: GetAllAssets, function returns all the current assets on the ledger');
-			let result = await contract.evaluateTransaction('GetAllAssets');
-			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-			// Now let's try to submit a transaction.
-			// This will be sent to both peers and if both peers endorse the transaction, the endorsed proposal will be sent
-			// to the orderer to be committed by each of the peer's to the channel ledger.
-			console.log('\n--> Submit Transaction: CreateAsset, creates new asset with ID, color, owner, size, and appraisedValue arguments');
-			result = await contract.submitTransaction('CreateAsset', 'asset13', 'yellow', '5', 'Tom', '1300');
-			console.log('*** Result: committed');
-			if (`${result}` !== '') {
-				console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-			}
-
-			console.log('\n--> Evaluate Transaction: ReadAsset, function returns an asset with a given assetID');
-			result = await contract.evaluateTransaction('ReadAsset', 'asset13');
-			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-			console.log('\n--> Evaluate Transaction: AssetExists, function returns "true" if an asset with given assetID exist');
-			result = await contract.evaluateTransaction('AssetExists', 'asset1');
-			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-			console.log('\n--> Submit Transaction: UpdateAsset asset1, change the appraisedValue to 350');
-			await contract.submitTransaction('UpdateAsset', 'asset1', 'blue', '5', 'Tomoko', '350');
-			console.log('*** Result: committed');
-
-			console.log('\n--> Evaluate Transaction: ReadAsset, function returns "asset1" attributes');
-			result = await contract.evaluateTransaction('ReadAsset', 'asset1');
-			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-			try {
-				// How about we try a transactions where the executing chaincode throws an error
-				// Notice how the submitTransaction will throw an error containing the error thrown by the chaincode
-				console.log('\n--> Submit Transaction: UpdateAsset asset70, asset70 does not exist and should return an error');
-				await contract.submitTransaction('UpdateAsset', 'asset70', 'blue', '5', 'Tomoko', '300');
-				console.log('******** FAILED to return an error');
-			} catch (error) {
-				console.log(`*** Successfully caught the error: \n    ${error}`);
-			}
-
-			console.log('\n--> Submit Transaction: TransferAsset asset1, transfer to new owner of Tom');
-			await contract.submitTransaction('TransferAsset', 'asset1', 'Tom');
-			console.log('*** Result: committed');
-
-			console.log('\n--> Evaluate Transaction: ReadAsset, function returns "asset1" attributes');
-			result = await contract.evaluateTransaction('ReadAsset', 'asset1');
-			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-			*/
-
 		} finally {
 			// Disconnect from the gateway when the application is closing
 			// This will close all connections to the network
@@ -192,4 +141,4 @@ async function main() {
 	}
 }
 
-main();
+//main();
--- server.js
+++ server.js
@@ -1,19 +1,37 @@
 const express = require('express');
 const app = express();
 
+const { init_connection, check_balance, mint } = require('./app.js');
+
+init_connection();
+
 app.set('view-engine', 'ejs');
 app.use(express.urlencoded({extended:false}));
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', {name: 'Cüzdan Uygulaması'})
+    res.render('index.ejs', {name: 'Dijital TL Uygulaması'});
 });
 
 app.post('/', (req, res) => {
-    console.log("1000");
+    if(req.body.button == 1){
+        console.log("Mint!");
+        mint('5000');
+        res.redirect('/');
+    } 
+    
+    if(req.body.button == 2){
+        console.log("Check Balance!");
+        check_balance().then(val => {
+            console.log(val);
+            res.render('view.ejs', {balance: 'bakiye=' + val});
+        }).catch(e => {
+            console.log(e);
+        });
+    }
 });
 
-app.get('/test', (req, res) => {
-    
+app.get('/view', (req, res) => {
+    res.render('view.ejs')
 });
 
 
