fac5828ca5247c7e8bffff1bebbb1df32cce3a3a
Bugrahan OZTURK <sbozturk97@gmail.com>
2022-01-30 10:55:18 -0800

    Added Login Page for the Application

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ const { buildCAClient, registerAndEnrollUser, enrollAdmin } = require('./CAUtil.
 const { buildCCPOrg1, buildWallet } = require('./AppUtil.js');
 
 const channelName = 'mychannel';
-const chaincodeName = 'erc20';
+const chaincodeName = 'token_erc20';
 const mspOrg1 = 'Org1MSP';
 const walletPath = path.join(__dirname, 'wallet');
-const org1UserId = 'appUser9';
+//const org1UserId = 'appUser9';
 
 var ccp;
 var caClient;
@@ -29,12 +29,18 @@ function prettyJSONString(inputString) {
 	return JSON.stringify(JSON.parse(inputString), null, 2);
 }
 
-exports.init_connection = async () => {
+exports.init_identity = async () => {
 	try {
-		
 		ccp = buildCCPOrg1();
 		caClient = buildCAClient(FabricCAServices, ccp, 'ca.org1.example.com');
 		wallet = await buildWallet(Wallets, walletPath);
+	} catch(error) {
+		console.error(`******** FAILED to initiliaze Identity for Hyperledger Fabric Connection: ${error}`);
+	}
+}
+
+exports.connect_network = async (org1UserId) => {
+	try {
 		await enrollAdmin(caClient, wallet, mspOrg1);
 		await registerAndEnrollUser(caClient, wallet, mspOrg1, org1UserId, 'org1.department1');
 
@@ -52,7 +58,7 @@ exports.init_connection = async () => {
 		// Get the contract from the network.
 		contract = network.getContract(chaincodeName);
 
-	} catch (error) {
+	} catch(error) {
 		console.error(`******** FAILED to initiliaze Hyperledger Fabric Connection: ${error}`);
 	}
 }
@@ -75,69 +81,6 @@ exports.mint = async (amount) => {
 		console.log('*** Result: committed');
 	} catch(error) {
 		console.error(`******** FAILED to mint tokens to the account: ${error}`)
-	}
-}
-
-exports.test = async () => {
-	try {
-		// build an in memory object with the network configuration (also known as a connection profile)
-		const ccp = buildCCPOrg1();
-
-		// build an instance of the fabric ca services client based on
-		// the information in the network configuration
-		const caClient = buildCAClient(FabricCAServices, ccp, 'ca.org1.example.com');
-
-		// setup the wallet to hold the credentials of the application user
-		const wallet = await buildWallet(Wallets, walletPath);
-
-		// in a real application this would be done on an administrative flow, and only once
-		await enrollAdmin(caClient, wallet, mspOrg1);
-
-		// in a real application this would be done only when a new user was required to be added
-		// and would be part of an administrative flow
-		
-		await registerAndEnrollUser(caClient, wallet, mspOrg1, org1UserId, 'org1.department1');
-
-		// Create a new gateway instance for interacting with the fabric network.
-		// In a real application this would be done as the backend server session is setup for
-		// a user that has been verified.
-		const gateway = new Gateway();
-
-		try {
-			// setup the gateway instance
-			// The user will now be able to create connections to the fabric network and be able to
-			// submit transactions and query. All transactions submitted by this gateway will be
-			// signed by this user using the credentials stored in the wallet.
-			await gateway.connect(ccp, {
-				wallet,
-				identity: org1UserId,
-				discovery: { enabled: true, asLocalhost: true } // using asLocalhost as this gateway is using a fabric network deployed locally
-			});
-
-			// Build a network instance based on the channel where the smart contract is deployed
-			const network = await gateway.getNetwork(channelName);
-
-			// Get the contract from the network.
-			const contract = network.getContract(chaincodeName);
-
-			console.log('\n--> Submit Transaction: Mint, creates new tokens and adds them to minter\'s account balance');
-			await contract.submitTransaction('Mint', '5000');
-			console.log('*** Result: committed');
-
-			console.log('\n--> Evaluate Transaction: ClientAccountBalance, function returns the balance of the given account');
-			let result = await contract.evaluateTransaction('ClientAccountBalance');
-			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-			let test = await contract.evaluateTransaction('ClientAccountID');
-			console.log(`*** Account_ID: ${test}`);
-
-		} finally {
-			// Disconnect from the gateway when the application is closing
-			// This will close all connections to the network
-			gateway.disconnect();
-		}
-	} catch (error) {
-		console.error(`******** FAILED to run the application: ${error}`);
 	}
 }
 
--- server.js
+++ server.js
@@ -1,38 +1,118 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
+const { init_identity, connect_network, check_balance, mint } = require('./app.js');
 
-const { init_connection, check_balance, mint } = require('./app.js');
+// Login Page Essentials 
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const initiliazePassport = require('./password-config.js');
 
-init_connection();
+initiliazePassport(
+    passport,
+    email => users.find(user => user.email == email),
+    id => users.find(user => user.id == id)
+);
+
+const users = [];
+/******************************************/
 
 app.set('view-engine', 'ejs');
 app.use(express.urlencoded({extended:false}));
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
 
-app.get('/', (req, res) => {
-    res.render('index.ejs', {name: 'Dijital TL Uygulaması'});
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', {name: req.user.email});
 });
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     if(req.body.button == 1){
         console.log("Mint!");
-        mint('5000');
+        await mint(parseInt(req.body.mint));
         res.redirect('/');
-    } 
-    
+    }     
     if(req.body.button == 2){
         console.log("Check Balance!");
-        check_balance().then(val => {
-            console.log(val);
-            res.render('view.ejs', {balance: 'bakiye=' + val});
-        }).catch(e => {
-            console.log(e);
-        });
+        res.redirect('/view')
     }
 });
 
-app.get('/view', (req, res) => {
-    res.render('view.ejs')
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs');
 });
 
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    failureRedirect: '/login',
+    failureFlash: true
+}), async (req, res) => {
+    try {
+        await connect_network(JSON.stringify(req.body.email));
+        res.redirect('/');
+    } catch {
+        res.redirect('/login');
+    }
+});
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs');
+});
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            email: req.body.email,
+            password: hashedPassword
+        })
+        init_identity();
+        res.redirect('/login');
+    } catch {
+        res.redirect('/register');
+    }
+});
+
+app.delete('/logout', (req, res) => {
+    req.logOut();
+    res.redirect('/login');
+});
+
+app.get('/view', async (req, res) => {
+    try {
+        let val = await check_balance();
+        res.render('view.ejs', {balance: val});
+    } catch(error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+function checkAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    return res.redirect('/login');
+}
+
+function checkNotAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return res.redirect('/');
+    }
+    next();
+}
 
 app.listen(3000);
